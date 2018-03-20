cacc6c0146e1f97de1b71df14f651b7c91506bec
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-20 02:22:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 var myArray = [];
 for(var i = 0; count < 25; count ++){
-  function addToArray(addStuff){
+  
     //var addMyStuff = 'I am ${count} strange loops.'
-    return myArray.push('I am ${i} strange loops.');
-  }
+    myArray.push('I am ${i} strange loops.');
+  
 }
