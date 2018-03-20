668493c67137d832c0e3c558821693c748d65d35
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-20 04:24:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,14 +1,14 @@
-
+var myArray =[];
 function forLoop(array) {
-var array = [];
+//var array = [];
 
     for (var i = 0; i < 25; i++) {
       if ( i === 0){
-        array.push("I am 1 strange loop.");
+        myArray.push("I am 1 strange loop.");
       } else {
-        array.push('I am '+ i + ' strange loop.');
+        myArray.push('I am '+ i + ' strange loop.');
       }
     }
 
-  return array;
+  return myArray;
 }
