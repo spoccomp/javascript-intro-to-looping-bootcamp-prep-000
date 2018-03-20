3286287fec2ab5facfaa140fe3e8329cb4428fa3
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-20 03:57:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ ffunction makeArray() {
   const array = [];
   const t = Math.floor(Math.random() * 10);
   for (var i = 0; i < t; i++) {
-    if ( i === 1){
+    if ( i === 0){
       array.push("I am 1 strange loop.");
     } else {
       array.push('I am'+ i + ' strange loop.');
