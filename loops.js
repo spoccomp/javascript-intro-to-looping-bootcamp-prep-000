765c1a97f767db370b6751277f585e696ac41874
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-20 02:21:39 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 var myArray = [];
-for(var count = 0; count < 25; count ++){
+for(var i = 0; count < 25; count ++){
   function addToArray(addStuff){
     //var addMyStuff = 'I am ${count} strange loops.'
-    return myArray.push('I am ${count} strange loops.')
+    return myArray.push('I am ${i} strange loops.');
   }
 }
