47aab8cd20582d7cee57c3a87e6f428dfa5f353e
Richard Trapanese <richardtrapanese@gmail.com>
2018-03-20 04:02:39 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function makeArray() {
   const array = [];
   const t = Math.floor(Math.random() * 10);
-  for (count = 0; count <=25; count++){
+  
     for (var i = 0; i < t; i++) {
       if ( i === 0){
         array.push("I am 1 strange loop.");
@@ -9,6 +9,6 @@ function makeArray() {
         array.push('I am'+ i + ' strange loop.');
       }
     }
-  }
+  
   return array;
 }
